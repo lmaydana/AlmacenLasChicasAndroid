a368fb65af4fc8485b0b779fc6b5dbc63ef6d5ac
lmaydana <lmaydana@fi.uba.ar>
2023-12-21 19:56:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pruebanegocio/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pruebanegocio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SearchView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startRequestsView(View view){
+        Intent requestIntent = new Intent(this, requestsActivity.class);
+        startActivity(requestIntent);
+    }
+}
---
+++ app/src/main/java/com/example/pruebanegocio/requestsActivity.java
@@ -0,0 +1,12 @@
+package com.example.pruebanegocio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class requestsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.requests);
+    }
+}
