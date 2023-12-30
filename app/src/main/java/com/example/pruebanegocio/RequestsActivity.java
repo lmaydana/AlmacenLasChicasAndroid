a3f290d8619f8f03eda8faf02a06304587c8179e
lmaydana <lmaydana@fi.uba.ar>
2023-12-30 14:30:40 -0300

    Continuing with grocery store app

--- app/src/main/java/com/example/pruebanegocio/RequestsActivity.java
+++ app/src/main/java/com/example/pruebanegocio/RequestsActivity.java
@@ -2,12 +2,15 @@ package com.example.pruebanegocio;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class RequestsActivity extends AppCompatActivity {
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -58,26 +62,33 @@ public class RequestsActivity extends AppCompatActivity {
             }
         });
 
+        Button deleteButton = findViewById(R.id.deleteStrikeThrough);
+        deleteButton.setOnClickListener(view -> {
+            MySqlConnection mySqlConnection = new MySqlConnection();
+            mySqlConnection.mysqlQueryWithoutResponse("DELETE FROM pedidos WHERE tachado = 1");
+            loadRequestsTable("");
+        });
+
     }
 
     private void loadRequestsTable(String whereProductLike ) {
         MySqlConnection mySqlConnectionReload = new MySqlConnection();
-        String query = "SELECT * FROM pedidos " + whereProductLike;
-        ArrayList<HashMap<String,String>> products = mySqlConnectionReload.mysqlQueryToArrayListOfObjects(query);
-        System.out.println("Array de productos:" + products);
+        String query = "SELECT * FROM pedidos " + whereProductLike+" ORDER BY id DESC";
+        ArrayList<HashMap<String,String>> requests = mySqlConnectionReload.mysqlQueryToArrayListOfObjects(query);
+        System.out.println("Array de productos:" + requests);
         ArrayList<String> colors = new ArrayList<>();
         colors.add("#0E0C0C");
         colors.add("#CC100F0F");
         int colorIndex = 0;
-        TableLayout productsTable = findViewById(R.id.requestTable);
-        productsTable.removeAllViews();
-        for (HashMap<String, String> product: products){
-            this.addNewRow(productsTable, product, colors.get(colorIndex%2));
+        TableLayout requestsTable = findViewById(R.id.requestTable);
+        requestsTable.removeAllViews();
+        for (HashMap<String, String> product: requests){
+            this.addNewRow(requestsTable, product, colors.get(colorIndex%2));
             colorIndex++;
         }
     }
 
-    private void addNewRow( TableLayout productsTable, HashMap<String, String> product, String color ) {
+    private void addNewRow( TableLayout productsTable, HashMap<String, String> requests, String color ) {
         TableRow newRow = new TableRow(this);
         newRow.setLayoutParams(new TableLayout.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         newRow.setBackgroundColor(Color.parseColor(color));
@@ -92,7 +103,7 @@ public class RequestsActivity extends AppCompatActivity {
         weights.put("proveedor", 3.0f);
         weights.put("precio", 1.0f);
         HashMap<String, TextView> rowView = new HashMap<>();
-        AutoCompleteTextView providers = this.getProvidersView(product);
+        AutoCompleteTextView providers = this.getProvidersView(requests);
         rowView.put("pedido", new TextView(this));
         rowView.put("producto_asociado", new TextView(this));
         rowView.put("proveedor", providers);
@@ -103,13 +114,31 @@ public class RequestsActivity extends AppCompatActivity {
             productAttribute.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, weights.get(key)));
             productAttribute.setGravity(Gravity.CENTER_VERTICAL);
             productAttribute.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
-            productAttribute.setText(product.get(key));
+            productAttribute.setText(requests.get(key));
             productAttribute.setTextColor(Color.parseColor("#ffffff"));
             newRow.addView(productAttribute);
         }
 
+        this.setStrikethrough(rowView.get("producto_asociado"), requests.get("tachado"));
         productsTable.addView(newRow, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
     }
+
+    private void setStrikethrough(TextView associatedProduct, String strikethrough) {
+        if( Boolean.parseBoolean(strikethrough) ){
+            associatedProduct.setPaintFlags(associatedProduct.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+        associatedProduct.setOnClickListener(view -> {
+            MySqlConnection mySqlConnection = new MySqlConnection();
+            if( (associatedProduct.getPaintFlags() & Paint.STRIKE_THRU_TEXT_FLAG) > 0) {
+                associatedProduct.setPaintFlags( associatedProduct.getPaintFlags() & (~ Paint.STRIKE_THRU_TEXT_FLAG));
+                mySqlConnection.mysqlQueryWithoutResponse("UPDATE pedidos SET tachado = 0 WHERE producto_asociado = '"+associatedProduct.getText().toString()+"'");
+            }else {
+                associatedProduct.setPaintFlags(associatedProduct.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                mySqlConnection.mysqlQueryWithoutResponse("UPDATE pedidos SET tachado = 1 WHERE producto_asociado = '"+associatedProduct.getText().toString()+"'");
+            }
+        });
+    }
+
 
     private AutoCompleteTextView getProvidersView(HashMap<String, String> product){
         AutoCompleteTextView providers = new AutoCompleteTextView(this);
